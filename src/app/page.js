ba25fa48fd2bfb3566056d4394451c80447ddd16
Sam <sambolton.kw@gmail.com>
2025-02-11 23:02:10 -0500

    setting up

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,21 @@ import { QRCode } from "./QRCode";
 export default function Home() {
   return (
     <div>
-      <QRCode />
+      <Challenge title="Challenge 1">
+        <QRCode />
+      </Challenge>
+      <Challenge title="Challenge 2"></Challenge>
+    </div>
+  );
+}
+
+function Challenge({ title, children }) {
+  return (
+    <div>
+      <h2 className="bg-black h-24 text-white text-center font-Figtree flex items-center justify-center">
+        {title}
+      </h2>
+      <p>{children}</p>
     </div>
   );
 }
