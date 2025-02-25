dc44e730a3bf2ea7086eed1948809d04814498ed
Sam <sambolton.kw@gmail.com>
2025-02-25 14:43:44 -0500

    removed qr

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import { Challenge } from "./Challenge";
-import { QRCode } from "./QRCode";
 import Calculator from "./mortgage-calculator-components/Calculator";
 import { CharacterCount } from "./characteCount-components/CharacterCount";
 import { Provider } from "react-redux";
@@ -10,15 +9,12 @@ import { store } from "../app/characteCount-components/store";
 export default function Home() {
   return (
     <div>
-      <Challenge title="Challenge 1" className="theme-project1">
-        <QRCode />
-      </Challenge>
-      <Challenge title="Challenge 2" className="theme-project2">
+      <Challenge title="Challenge 1" className="theme-project2">
         <Calculator />
       </Challenge>
 
       <Challenge
-        title="🚧 Challenge 3 🚧 Under Construction 🚧"
+        title="🚧 Challenge 2 🚧 Under Construction 🚧"
         className="theme-project3"
       >
         <Provider store={store}>
